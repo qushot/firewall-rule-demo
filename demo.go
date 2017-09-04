8b7717723b9fcbcb3516f2b06e1a8e0aa6a7dcaf
qushot <moto@yoshidumi.co.jp>
2017-09-04 17:29:08 +0900

    add log print IP Address

--- demo.go
+++ demo.go
@@ -1,14 +1,20 @@
 package demo
 
 import (
+	"google.golang.org/appengine"
+
 	"fmt"
 	"net/http"
+
+	"google.golang.org/appengine/log"
 )
 
-func root(w http.ResponseWriter, r *http.Request) {
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	log.Infof(ctx, "Request from IP Address \"%v\"\n", r.RemoteAddr)
 	fmt.Fprintln(w, "hello")
 }
 
 func init() {
-	http.HandleFunc("/", root)
+	http.HandleFunc("/", rootHandler)
 }
